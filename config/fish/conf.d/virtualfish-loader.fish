3c2a9cd20c8bbca2d2dfd3680af30e4fad08e7c8
mystikspiral21 <circleinflames21@gmail.com>
2026-06-27 11:01:26 -0500

    Initial dotfiles commit - Sway CachyOS setup

---
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.5.9
+set -g VIRTUALFISH_PYTHON_EXEC /home/benji/Music/e2aconv/audiobook-env/bin/python3
+source /home/benji/Music/e2aconv/audiobook-env/lib/python3.13/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -gx QT_QPA_PLATFORMTHEME qt6ct
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
